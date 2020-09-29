0917c0934a0bd025d3f66acf3a48a81c192006dc
Suman Jha <meetsumanat@gmail.com>
2020-09-28 21:49:29 -0700

    setting up firebase in the project

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyA-lUgerrQx34AL77H-I_niAKpgVP_VfIg',
+  authDomain: 'facebook-e26bc.firebaseapp.com',
+  databaseURL: 'https://facebook-e26bc.firebaseio.com',
+  projectId: 'facebook-e26bc',
+  storageBucket: 'facebook-e26bc.appspot.com',
+  messagingSenderId: '120471548344',
+  appId: '1:120471548344:web:894252cbaabd9441dedf4d',
+  measurementId: 'G-7PHPX0HGYS',
+};
